3f9ffa1ac442f673aa13283b9ebdbbaaef5672a1
itxelijah07 <itxelijah07@gmail.com>
2025-08-13 13:56:52 +0500

    Update bot.js

--- Core/bot.js
+++ Core/bot.js
@@ -1,5 +1,3 @@
-fix
-
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, getAggregateVotesInPollMessage, isJidNewsletter, delay, proto } = require('@whiskeysockets/baileys');
 const { Boom } = require('@hapi/boom');
 const qrcode = require('qrcode-terminal');
