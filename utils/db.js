fd1c526d08ff941efbe63fa6eb0914a6c98d96ea
itxelijah07 <itxelijah07@gmail.com>
2025-08-13 14:02:20 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,26 @@
-const config = require('../config');
+const configPromise = require('../config'); // This is a Promise from your Config class
 const { MongoClient } = require('mongodb');
 
-const MONGO_URI = config.mongo.uri;
-const DB_NAME = config.mongo.dbName;
-const OPTIONS = config.mongo.options;
-
-const client = new MongoClient(MONGO_URI, OPTIONS);
+let client; // Will hold the MongoDB client instance
 
 async function connectDb() {
+    // Wait for config to finish loading
+    const config = await configPromise;
+
+    const MONGO_URI = config.get('mongo.uri');
+    const DB_NAME = config.get('mongo.dbName');
+    const OPTIONS = config.get('mongo.options') || {};
+
+    // Create the client if it doesn't exist yet
+    if (!client) {
+        client = new MongoClient(MONGO_URI, OPTIONS);
+    }
+
+    // Connect if not connected
     if (!client.topology?.isConnected()) {
         await client.connect();
     }
+
     return client.db(DB_NAME);
 }
 
