bc8ed50247d84b1a4bc1266c93c837c2f95aa8fd
itxelijah07 <itxelijah07@gmail.com>
2025-08-13 14:08:57 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,44 +1,17 @@
-
-const { MongoClient } = require('mongodb');
 const config = require('../config');
-const logger = require('../Core/logger');
+const { MongoClient } = require('mongodb');
 
-let client;
-let db;
+const MONGO_URI = config.get('mongo.uri');
+const DB_NAME = config.get('mongo.dbName');
+const OPTIONS = config.get('mongo.options');
+
+const client = new MongoClient(MONGO_URI, OPTIONS);
 
 async function connectDb() {
-    if (db) return db;
-
-    // Try to get URI from config or environment
-    const uri = config.get('mongo.uri') || process.env.MONGO_URI;
-    if (!uri) {
-        throw new Error('MongoDB URI is not set in config or environment');
-    }
-
-    const dbName = config.get('mongo.dbName') || process.env.MONGO_DB || 'hyperwa';
-
-    try {
-        client = new MongoClient(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+    if (!client.topology?.isConnected()) {
         await client.connect();
-        db = client.db(dbName);
-        logger.info(`✅ Connected to MongoDB database: ${dbName}`);
-        return db;
-    } catch (err) {
-        logger.error('❌ MongoDB connection error:', err.message);
-        throw err;
     }
+    return client.db(DB_NAME);
 }
 
-async function closeDb() {
-    if (client) {
-        await client.close();
-        logger.info('🔌 MongoDB connection closed');
-        client = null;
-        db = null;
-    }
-}
-
-module.exports = { connectDb, closeDb };
+module.exports = { connectDb };
