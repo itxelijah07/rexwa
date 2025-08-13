d7bd44b361fa8c8db4a6af53d90a9fdcec2f35ff
itxelijah07 <itxelijah07@gmail.com>
2025-08-13 14:05:12 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,27 +1,28 @@
-const configPromise = require('../config'); // This is a Promise from your Config class
+// utils/db.js
+const config = require('../config');
 const { MongoClient } = require('mongodb');
 
-let client; // Will hold the MongoDB client instance
+const MONGO_URI = config.mongo?.uri || process.env.MONGO_URI;
+const DB_NAME = config.mongo?.dbName || process.env.MONGO_DB_NAME || 'hyperwa';
+const OPTIONS = config.mongo?.options || {};
+
+if (!MONGO_URI) {
+    console.error('❌ MongoDB URI is missing! Set it in config.js or MONGO_URI env variable.');
+    process.exit(1);
+}
+
+const client = new MongoClient(MONGO_URI, OPTIONS);
 
 async function connectDb() {
-    // Wait for config to finish loading
-    const config = await configPromise;
-
-    const MONGO_URI = config.get('mongo.uri');
-    const DB_NAME = config.get('mongo.dbName');
-    const OPTIONS = config.get('mongo.options') || {};
-
-    // Create the client if it doesn't exist yet
-    if (!client) {
-        client = new MongoClient(MONGO_URI, OPTIONS);
+    try {
+        if (!client.topology?.isConnected()) {
+            await client.connect();
+        }
+        return client.db(DB_NAME);
+    } catch (err) {
+        console.error('❌ Failed to connect to MongoDB:', err);
+        throw err;
     }
-
-    // Connect if not connected
-    if (!client.topology?.isConnected()) {
-        await client.connect();
-    }
-
-    return client.db(DB_NAME);
 }
 
 module.exports = { connectDb };
